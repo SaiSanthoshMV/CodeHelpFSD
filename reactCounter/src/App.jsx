8a2dacb8a8b8ad7c94de889875e78ea1e38afba8
SaiSanthoshMV <mvs.sai999@gmail.com>
2025-07-03 18:19:30 +0530

    commit2

---
+++ reactCounter/src/App.jsx
@@ -0,0 +1,15 @@
+// counter
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p> Count is {count}</p>
+      <button onClick={() => setCount(count+1)}>click</button>
+    </>
+  )
+}
+
+export default App
