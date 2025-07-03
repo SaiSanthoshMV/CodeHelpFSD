cf193ef9c41ce77848be537a76f6aca0b14f1c14
SaiSanthoshMV <mvs.sai999@gmail.com>
2025-07-03 19:04:09 +0530

    first project with simple text

---
+++ react-project-one/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 >
+        Hey Maan ! Whatsaaappp
+      </h1>
+    </>
+  )
+}
+
+export default App
